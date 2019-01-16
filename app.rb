e8051f826690df4703f0a34893ab73adbae4e5ca
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-16 20:39:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+    #a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
     200
     @sentence = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
   end
